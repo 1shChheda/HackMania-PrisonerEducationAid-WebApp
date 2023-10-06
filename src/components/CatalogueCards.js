84cbefcb23672f3bf0cd5a03d50a6ea38d90ecc6
Kimaya Ved <102381197+kimayav@users.noreply.github.com>
2023-10-07 03:32:14 +0530

    catalogue updated

--- src/components/CatalogueCards.js
+++ src/components/CatalogueCards.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import img from '../images/courses.png';
-import img2 from '../images/podcast.png';
-import img3 from '../images/articles.png';
-import img4 from '../images/jobs.png';
+import img from '../images/2468081.png';
+import img2 from '../images/4029013.png';
+import img3 from '../images/88167.png';
+import img4 from '../images/942800.png';
+import img5 from '../images/quiz.png';
+import img6 from '../images/5351504.png';
 
 const Services = () => {
 
@@ -10,8 +12,8 @@ const Services = () => {
         <div id="services" className="bg-gray-100 py-12" >
             <section data-aos="zoom-in-down">
                 <div className="my-4 py-4">
-                    <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Catalogue</h2>
-                    <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">We are deeply committed to the growth and success of our clients.</h2>
+                    <h2 className="my-2 text-center text-3xl text-black-900 uppercase font-bold">Catalogue</h2>
+                    <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">We are deeply committed to the growth and improvement.</h2>
                 </div>
 
                 <div className="px-12" data-aos="fade-down" data-aos-delay="600">
@@ -51,6 +53,26 @@ const Services = () => {
                             <div className="m-2 text-justify text-sm">
                                 <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img4} />
                                 <h2 className="font-semibold my-4 text-2xl text-center ">Jobs</h2>
+                                <p className="text-md font-medium">
+                                    We provide domain registration and web hosting services to individuals and organizations to enable them gain visibility in the digital space.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                            <div className="m-2 text-justify text-sm">
+                                <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img5} />
+                                <h2 className="font-semibold my-4 text-2xl text-center ">Quizzes</h2>
+                                <p className="text-md font-medium">
+                                    We provide domain registration and web hosting services to individuals and organizations to enable them gain visibility in the digital space.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                            <div className="m-2 text-justify text-sm">
+                                <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img6} />
+                                <h2 className="font-semibold my-4 text-2xl text-center ">Upcoming Webinars</h2>
                                 <p className="text-md font-medium">
                                     We provide domain registration and web hosting services to individuals and organizations to enable them gain visibility in the digital space.
                                 </p>
