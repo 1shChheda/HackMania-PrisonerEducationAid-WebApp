afbe8a0e10fa7570690231253d4b40da372853c8
darshil929 <darshilt929@gmail.com>
2023-10-07 11:30:15 +0530

    Landing page changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Cta from '../components/Cta';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
         <>
             <Hero />
             <Services />
-            <Portfolio />
             <Cta/>
             <Footer />
         </>
